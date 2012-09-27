474223ec5b7c368bde377650b70c4e3f95bc1b85
delphinus35 <delphinus@remora.cx>
2012-09-28 08:49:15 +0900

    apple-touch-icon support

--- lib/WebAppIconMaker/Web/Dispatcher.pm
+++ lib/WebAppIconMaker/Web/Dispatcher.pm
@@ -9,7 +9,7 @@ use Digest::MD5 qw!md5_hex!;
 use Encode;
 use JSON;
 use Log::Minimal;
-use LWP::Simple qw!!;
+use LWP::UserAgent;
 
 my $top = '';
 my %sites = (
@@ -55,15 +55,15 @@ get "$top/getAddress" => sub { my $c = shift;
 };
 
 get "$top/img/{id}" => sub { my ($c, $args) = @_;
-    my ($data) = $c->dbh->selectrow_array(<<SQL, undef, $args->{id});
-        SELECT content FROM icons WHERE id = ?;
+    my ($ext, $data) = $c->dbh->selectrow_array(<<SQL, undef, $args->{id});
+        SELECT ext, content FROM icons WHERE id = ?;
 SQL
 
     if ($data) {
         $c->create_response(
             200,
             [
-                'Content-Type' => 'image/ico',
+                'Content-Type' => "image/$ext",
                 'Content-Length' => length $data,
             ],
             [$data],
@@ -76,8 +76,10 @@ SQL
 sub _get_title { my $c = shift;
     my $site_url = shift;
 
-    my $content = LWP::Simple::get($site_url);
-    my ($title) = $content =~ m!<title>([^<]+)</title>!;
+    my $ua = LWP::UserAgent->new(agent => 'AppleWebKit/536.26'
+        . ' (KHTML, like Gecko) Version/6.0 Mobile/10A405 Safari/8536.25');
+    my $res = $ua->get($site_url);
+    my ($title) = $res->decoded_content =~ m!<title>([^<]+)</title>!;
 
     return $title;
 }
@@ -85,7 +87,38 @@ sub _get_title { my $c = shift;
 sub _get_favicon { my $c = shift;
     my $site_url = shift;
     my ($top_url) = $site_url =~ m!^(https?://[^/]+)!;
-    my $icon_url = "$top_url/favicon.ico";
+    my @icons = qw!
+        apple-touch-icon-precomposed.png
+        apple-touch-icon.png
+        favicon.ico
+    !;
+
+    my $ua = LWP::UserAgent->new(agent => 
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) '
+        . 'AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 '
+        . 'Mobile/10A405 Safari/8536.25');
+    my $res = $ua->get($site_url);
+    my $icon_url;
+    if ($res->content) {
+        my ($apple_touch_icon) = $res->content =~ m!
+            <link\s*rel="apple-touch-icon"\s*href="([^"]+)"
+        !x;
+        $icon_url = $apple_touch_icon if $apple_touch_icon;
+    }
+    unless ($icon_url) {
+        for my $i (@icons) {
+            my $res = $ua->head("$top_url/$i");
+            infof($res->header('Content-Type'));
+            if ($res->header('Content-Type') =~ /image/) {
+                $icon_url = "$top_url/$i";
+                last;
+            }
+        }
+    }
+    unless ($icon_url) {
+        return;
+    }
+    infof($icon_url);
 
     my $md5 = md5_hex($icon_url);
 
@@ -94,14 +127,17 @@ sub _get_favicon { my $c = shift;
 SQL
 
     unless ($count) {
-        my $icon = LWP::Simple::get($icon_url);
+        my $res = $ua->get($icon_url);
+        my ($ext) = $icon_url =~ /[^.]+$/;
+
         my $sth = $c->dbh->prepare(<<SQL);
-            INSERT INTO icons VALUES (?, ?, ?);
+            INSERT INTO icons VALUES (?, ?, ?, ?);
 SQL
 
         $sth->bind_param(1, $md5, SQL_VARCHAR);
-        $sth->bind_param(2, $icon, SQL_BLOB);
-        $sth->bind_param(3, time, SQL_INTEGER);
+        $sth->bind_param(2, $ext, SQL_VARCHAR);
+        $sth->bind_param(3, $res->content, SQL_BLOB);
+        $sth->bind_param(4, time, SQL_INTEGER);
         $sth->execute;
     }
 
