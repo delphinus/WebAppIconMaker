4ca39225eb84495c10b112b09891ac2ee0e6f06d
delphinus35 <delphinus@remora.cx>
2012-09-28 22:16:27 +0900

    several urls

--- config/deployment.pl
+++ config/deployment.pl
@@ -1,6 +1,10 @@
 use File::Spec;
 use File::Basename qw(dirname);
 my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
+my $sites = do {
+    local $/;
+    YAML::LoadFile(File::Spec->catfile($basedir, 'config', 'sites.yaml'));
+};
 my $dbpath;
 if ( -d '/home/dotcloud/') {
     $dbpath = "/home/dotcloud/deployment.db";
@@ -9,6 +13,7 @@ if ( -d '/home/dotcloud/') {
 }
 +{
     app_url => 'http://waim.remora.cx',
+    sites => $sites,
     'DBI' => [
         "dbi:SQLite:dbname=$dbpath",
         '',
