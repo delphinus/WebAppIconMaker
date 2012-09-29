1b77f14a144ca1f3761504ea5fbe6a2241206c1d
delphinus35 <delphinus@remora.cx>
2012-09-29 10:55:30 +0900

    use local hostname in development

--- config/development.pl
+++ config/development.pl
@@ -1,11 +1,13 @@
 use File::Spec;
 use File::Basename qw(dirname);
+use Sys::Hostname;
 use YAML;
 my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
 my $sites = do {
     local $/;
     YAML::LoadFile(File::Spec->catfile($basedir, 'config', 'sites.yaml'));
 };
+my $address = sprintf '%vd', scalar gethostbyname hostname();
 my $dbpath;
 if ( -d '/home/dotcloud/') {
     $dbpath = "/home/dotcloud/development.db";
@@ -13,7 +15,7 @@ if ( -d '/home/dotcloud/') {
     $dbpath = File::Spec->catfile($basedir, 'db', 'development.db');
 }
 +{
-    app_url => 'http://192.168.40.66:5000',
+    app_url => "http://$address:5000",
     sites => $sites,
     'DBI' => [
         "dbi:SQLite:dbname=$dbpath",
